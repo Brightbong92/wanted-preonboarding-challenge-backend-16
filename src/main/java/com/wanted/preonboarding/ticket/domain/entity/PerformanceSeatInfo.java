7176f7fabbe1ad3dc2318779cf4728c9937a9982
장현봉 <wkdgusqhd080@naver.com>
2024-01-22 10:54:43 +0900

    UPDATE PerformanceSeatInfo Entity

--- src/main/java/com/wanted/preonboarding/ticket/domain/entity/PerformanceSeatInfo.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/entity/PerformanceSeatInfo.java
@@ -1,4 +1,31 @@
 package com.wanted.preonboarding.ticket.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PerformanceSeatInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(columnDefinition = "BINARY(16)", nullable = false, name = "performance_id")
+    private UUID performanceId;
+    @Column(nullable = false)
+    private int round;
+    @Column(nullable = false)
+    private int gate;
+    @Column(nullable = false)
+    private char line;
+    @Column(nullable = false)
+    private int seat;
+    @Column(nullable = false, name = "is_reserve", columnDefinition = "varchar(255) default 'disable'")
+    private String isReverse;
 }
